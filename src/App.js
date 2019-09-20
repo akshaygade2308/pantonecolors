11e1825d67f6f0b01b97a5ae8ed88af420251e31
akshaygade2308 <akshg7@gmail.com>
2019-09-20 11:35:57 +0200

    color and hex displayed on click

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component{
   render(){
     return(
       <div>
-        <Color consoleColor={this.getColor} noOfColorsToRender = {this.props.noOfColorsToRender || 84} widthProp = {this.props.widthProp || 40} heightProp = {this.props.heightProp || 25}></Color>
+        <Color selectedColor={this.state.selectedColor} consoleColor={this.getColor} noOfColorsToRender = {this.props.noOfColorsToRender || 84} widthProp = {this.props.widthProp || 40} heightProp = {this.props.heightProp || 25} />
       </div>
     );
 
--- src/Color.js
+++ src/Color.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
-// import Button from 'antd/es/button';
 import { Popover } from 'antd';
 import Scrollbar from './Scrollbar';
 import Searchbox from './Searchbox';
 import 'bootstrap/dist/css/bootstrap.css';
-// import AOS from 'aos';
-// import '../node_modules/aos/dist/aos.css'; 
+
 const palatte = require(`!raw-loader!./Images/colorplate.svg`);
 
 class Color extends Component{
@@ -16,12 +14,14 @@ class Color extends Component{
         this.state = {
             colors : [],
             searchfield : '',
-            colorsToRender : this.props.noOfColorsToRender
+            colorsToRender : this.props.noOfColorsToRender,
+            
             // showHex : false,
             // showColors : false
         }
         this.fetch()
         // this.colorOnClick = this.colorOnClick.bind(this)
+        this.hexToRgb = this.hexToRgb.bind(this)        
     }
   
     fetch(){
@@ -46,63 +46,93 @@ class Color extends Component{
         })
     }
 
+    hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        } : {};
+      }
+
     // colorOnClick(hex) {
     //     console.log('hex', hex)
     //     // this.props.consoleHex(hex)
     // }
 
-    render(){   
+    render(){  
         const {colors, searchfield} = this.state;
         const filteredColors = colors.filter(color => {
         return color.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
         const content = (
-        <div className = "row">    
-        <div className = "contentdiv col-xs-12 col-sm-4">      
-            <Searchbox searchChange = {this.onSearchChange} />
-            
-                <div>
-                <Scrollbar>
-                {
-                filteredColors.slice(0,this.state.colorsToRender).map(col=>(
-                    <div className = "filterdiv" title = {col.hexvalue} onWheel = {(e) => this.handleClick(e)} style={{backgroundColor:col.hexvalue, display: 'inline-block', width : this.props.widthProp, height : this.props.heightProp, borderRadius : '3px', marginLeft : '7px',
-                        boxShadow : '3px 2px 2px gray', padding : '0px'}} 
-                        onClick={() => this.props.consoleColor(col)}
-                        // onClick={this.props.hexShow}
-                        >
-                        {/* <button onClick={this._showHex.bind(null, true)}>show</button>
-                        <button onClick={this._showHex.bind(null, false)}>hide</button> */}
-                        {/* { this.state.showHex && (<div>{col.hexvalue}</div>) } */}
-                    </div>     
-                ))
-                }
-                </Scrollbar>
+            <div className = "row">    
+                <div className = "contentdiv col-xs-12 col-sm-8">   
+                <div className = "row">
+                   <div className = "col-xs-12 col-sm-6">
+                   <Searchbox searchChange = {this.onSearchChange} />
+                        <div>
+                            <Scrollbar>
+                            {
+                            filteredColors.slice(0,this.state.colorsToRender).map(col=>(
+                                <div className = "filterdiv" title = {col.hexvalue} onWheel = {(e) => this.handleClick(e)} style={{backgroundColor:col.hexvalue, display: 'inline-block', width : this.props.widthProp, height : this.props.heightProp, marginLeft : '7px',
+                                    padding : '0px'}} 
+                                    onClick={() => this.props.consoleColor(col)}
+                                    // onClick={this.props.hexShow}
+                                    >
+                                    {/* <button onClick={this._showHex.bind(null, true)}>show</button>
+                                    <button onClick={this._showHex.bind(null, false)}>hide</button> */}
+                                    {/* { this.state.showHex && (<div>{col.hexvalue}</div>) } */}
+                                </div>     
+                            ))
+                            }
+
+                            </Scrollbar>
+                        </div>
+                    </div>
+                    {this.props.selectedColor.hexvalue ? <div className = "col-xs-12 col-sm-2" style = {{marginLeft: '0px', paddingLeft : '10px'}}>
+                    <div className = "row" style = {{height: '130px', backgroundColor: this.props.selectedColor.hexvalue || ''}}>
+                        {/* <div>
+                            {this.props.selectedColor.hexvalue}
+                        </div> */}
+                    </div>
+                    <div className = "row" style = {{paddingTop : '10px', justifyContent : 'center', fontSize : '15px', fontFamily : 'sans-serif'}}>
+                        {this.props.selectedColor.hexvalue}
+                    </div>
+                    <div className = "row" style = {{alignItems : 'center', justifyContent : 'center', fontSize : '15px', fontFamily : 'sans-serif'}}>
+                        <div style = {{paddingLeft : '2px', display : 'inline-block'}}>r:{this.hexToRgb(this.props.selectedColor.hexvalue) && this.hexToRgb(this.props.selectedColor.hexvalue).r}</div>
+                        <div style = {{paddingLeft : '2px', display : 'inline-block'}}>g:{this.hexToRgb(this.props.selectedColor.hexvalue) && this.hexToRgb(this.props.selectedColor.hexvalue).g}</div>
+                        <div style = {{paddingLeft : '2px', display : 'inline-block'}}>b:{this.hexToRgb(this.props.selectedColor.hexvalue) && this.hexToRgb(this.props.selectedColor.hexvalue).b}</div>
+                    </div>
+                </div> : null}
+                    
+                </div>   
+                    
+                        {/* <button className = "viewButton" onClick = {() =>this.handleClick()}>View More</button> */}
+
+                </div>
             </div>
-            {/* <button className = "viewButton" onClick = {() =>this.handleClick()}>View More</button> */}
-        </div>
-        <div className = "col-xs-12 col-sm-8" />
-        </div>
         );
         
-    // console.log(palatte)
+        // console.log(palatte)
     
-    if(colors.length === 0){
-        return <h1>Loading</h1>
-    }else{
-        return (
-            <div>
-            <div className = "popdiv">
-            
-            <Popover className = "pop" placement="bottom" content={content} trigger="click">
-                <div className = "palettediv" dangerouslySetInnerHTML = {{__html : palatte.default}}  /* onClick={() => this.handleClicks()} *//>
-            </Popover>
+        if(colors.length === 0){
+            return <h1>Loading</h1>
+        }else{
+            return (
+                <div>
+                    <div className = "popdiv">
+                    
+                        <Popover className = "pop" placement="bottom" content={content} trigger="click">
+                            <div className = "palettediv" dangerouslySetInnerHTML = {{__html : palatte.default}}  /* onClick={() => this.handleClicks()} *//>
+                        </Popover>
 
-            </div>
-            </div>
-        )
+                    </div>
+                </div>
+            )
+        }
     }
-}
 }
 
 export default Color;
